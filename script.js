5850e54520438e2b797463482270c808a4de748f
Maxim Avvakumov <reamonn8@gmail.com>
2023-04-30 21:45:34 +0500

    feat: add structur keyboard

---
+++ script.js
@@ -0,0 +1,935 @@
+const keysEng = [
+  {
+    Backquote: {
+      value: '`',
+      altValue: '~',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit1: {
+      value: '1',
+      altValue: '!',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit2: {
+      value: '2',
+      altValue: '@',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit3: {
+      value: '3',
+      altValue: '#',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit4: {
+      value: '4',
+      altValue: '$',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit5: {
+      value: '5',
+      altValue: '%',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit6: {
+      value: '6',
+      altValue: '^',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit7: {
+      value: '7',
+      altValue: '&',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit8: {
+      value: '8',
+      altValue: '*',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit9: {
+      value: '9',
+      altValue: '(',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit0: {
+      value: '0',
+      altValue: ')',
+      isCapitals: false,
+    },
+  },
+  {
+    Minus: {
+      value: '-',
+      altValue: '_',
+      isCapitals: false,
+    },
+  },
+  {
+    Equal: {
+      value: '=',
+      altValue: '+',
+      isCapitals: false,
+    },
+  },
+  {
+    Backspace: {
+      value: '&larr;',
+      altValue: '&larr;',
+      isCapitals: false,
+    },
+  },
+  {
+    Tab: {
+      value: 'Tab',
+      altValue: 'Tab',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyQ: {
+      value: 'q',
+      altValue: 'Q',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyW: {
+      value: 'w',
+      altValue: 'W',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyE: {
+      value: 'e',
+      altValue: 'E',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyR: {
+      value: 'r',
+      altValue: 'R',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyT: {
+      value: 't',
+      altValue: 'T',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyY: {
+      value: 'y',
+      altValue: 'Y',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyU: {
+      value: 'u',
+      altValue: 'U',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyI: {
+      value: 'i',
+      altValue: 'I',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyO: {
+      value: 'o',
+      altValue: 'O',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyP: {
+      value: 'p',
+      altValue: 'P',
+      isCapitals: false,
+    },
+  },
+  {
+    BracketLeft: {
+      value: '[',
+      altValue: '{',
+      isCapitals: false,
+    },
+  },
+  {
+    BracketRight: {
+      value: ']',
+      altValue: '}',
+      isCapitals: false,
+    },
+  },
+  {
+    Backslash: {
+      value: '&#92',
+      altValue: '|',
+      isCapitals: false,
+    },
+  },
+  {
+    Delete: {
+      value: 'Del',
+      altValue: 'Del',
+      isCapitals: false,
+    },
+  },
+  {
+    CapsLock: {
+      value: 'Caps Lock',
+      altValue: 'Caps Lock',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyA: {
+      value: 'a',
+      altValue: 'A',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyS: {
+      value: 's',
+      altValue: 'S',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyD: {
+      value: 'd',
+      altValue: 'D',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyF: {
+      value: 'f',
+      altValue: 'F',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyG: {
+      value: 'g',
+      altValue: 'G',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyH: {
+      value: 'h',
+      altValue: 'H',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyJ: {
+      value: 'j',
+      altValue: 'J',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyK: {
+      value: 'k',
+      altValue: 'K',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyL: {
+      value: 'l',
+      altValue: 'L',
+      isCapitals: false,
+    },
+  },
+  {
+    Semicolon: {
+      value: ';',
+      altValue: ':',
+      isCapitals: false,
+    },
+  },
+  {
+    Quote: {
+      value: "'",
+      altValue: '"',
+      isCapitals: false,
+    },
+  },
+  {
+    Enter: {
+      value: 'Enter',
+      altValue: 'Enter',
+      isCapitals: false,
+    },
+  },
+  {
+    ShiftLeft: {
+      value: 'Shift',
+      altValue: 'Shift',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyZ: {
+      value: 'z',
+      altValue: 'Z',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyX: {
+      value: 'x',
+      altValue: 'X',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyC: {
+      value: 'c',
+      altValue: 'C',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyV: {
+      value: 'v',
+      altValue: 'V',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyB: {
+      value: 'b',
+      altValue: 'B',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyN: {
+      value: 'n',
+      altValue: 'N',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyM: {
+      value: 'm',
+      altValue: 'M',
+      isCapitals: false,
+    },
+  },
+  {
+    Comma: {
+      value: ',',
+      altValue: '<',
+      isCapitals: false,
+    },
+  },
+  {
+    Period: {
+      value: '.',
+      altValue: '>',
+      isCapitals: false,
+    },
+  },
+  {
+    Slash: {
+      value: '/',
+      altValue: '?',
+      isCapitals: false,
+    },
+  },
+  {
+    ArrowUp: {
+      value: '&uarr;',
+      altValue: '&uarr;',
+      isCapitals: false,
+    },
+  },
+  {
+    ShiftRight: {
+      value: 'Shift',
+      altValue: 'Shift',
+      isCapitals: false,
+    },
+  },
+  {
+    ControlLeft: {
+      value: 'Ctrl',
+      altValue: 'Ctrl',
+      isCapitals: false,
+    },
+  },
+  {
+    MetaLeft: {
+      value: 'Win',
+      altValue: 'Win',
+      isCapitals: false,
+    },
+  },
+  {
+    AltLeft: {
+      value: 'Alt',
+      altValue: 'Alt',
+      isCapitals: false,
+    },
+  },
+  {
+    Space: {
+      value: 'Space',
+      altValue: 'Space',
+      isCapitals: false,
+    },
+  },
+  {
+    AltRight: {
+      value: 'Alt',
+      altValue: 'Alt',
+      isCapitals: false,
+    },
+  },
+  {
+    ArrowLeft: {
+      value: '&larr;',
+      altValue: '&larr;',
+      isCapitals: false,
+    },
+  },
+  {
+    ArrowDown: {
+      value: '&darr;',
+      altValue: '&darr;',
+      isCapitals: false,
+    },
+  },
+  {
+    ArrowRight: {
+      value: '&rarr;',
+      altValue: '&rarr;',
+      isCapitals: false,
+    },
+  },
+  {
+    ControlRight: {
+      value: 'Ctrl',
+      altValue: 'Ctrl',
+      isCapitals: false,
+    },
+  },
+];
+const keysRu = [
+  {
+    Backquote: {
+      value: 'ё',
+      altValue: 'Ё',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit1: {
+      value: '1',
+      altValue: '!',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit2: {
+      value: '2',
+      altValue: '"',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit3: {
+      value: '3',
+      altValue: '№',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit4: {
+      value: '4',
+      altValue: ';',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit5: {
+      value: '5',
+      altValue: '%',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit6: {
+      value: '6',
+      altValue: ':',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit7: {
+      value: '7',
+      altValue: '?',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit8: {
+      value: '8',
+      altValue: '*',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit9: {
+      value: '9',
+      altValue: '(',
+      isCapitals: false,
+    },
+  },
+  {
+    Digit0: {
+      value: '0',
+      altValue: ')',
+      isCapitals: false,
+    },
+  },
+  {
+    Minus: {
+      value: '-',
+      altValue: '_',
+      isCapitals: false,
+    },
+  },
+  {
+    Equal: {
+      value: '=',
+      altValue: '+',
+      isCapitals: false,
+    },
+  },
+  {
+    Backspace: {
+      value: '&larr;',
+      altValue: '&larr;',
+      isCapitals: false,
+    },
+  },
+  {
+    Tab: {
+      value: 'Tab',
+      altValue: 'Tab',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyQ: {
+      value: 'й',
+      altValue: 'Й',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyW: {
+      value: 'ц',
+      altValue: 'Ц',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyE: {
+      value: 'у',
+      altValue: 'У',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyR: {
+      value: 'к',
+      altValue: 'К',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyT: {
+      value: 'е',
+      altValue: 'Е',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyY: {
+      value: 'н',
+      altValue: 'Н',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyU: {
+      value: 'г',
+      altValue: 'Г',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyI: {
+      value: 'ш',
+      altValue: 'Ш',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyO: {
+      value: 'щ',
+      altValue: 'Щ',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyP: {
+      value: 'з',
+      altValue: 'З',
+      isCapitals: false,
+    },
+  },
+  {
+    BracketLeft: {
+      value: 'х',
+      altValue: 'Х',
+      isCapitals: false,
+    },
+  },
+  {
+    BracketRight: {
+      value: 'ъ',
+      altValue: 'Ъ',
+      isCapitals: false,
+    },
+  },
+  {
+    Backslash: {
+      value: '&#92',
+      altValue: '|',
+      isCapitals: false,
+    },
+  },
+  {
+    Delete: {
+      value: 'Del',
+      altValue: 'Del',
+      isCapitals: false,
+    },
+  },
+  {
+    CapsLock: {
+      value: 'Caps Lock',
+      altValue: 'Caps Lock',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyA: {
+      value: 'ф',
+      altValue: 'Ф',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyS: {
+      value: 'ы',
+      altValue: 'Ы',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyD: {
+      value: 'в',
+      altValue: 'В',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyF: {
+      value: 'а',
+      altValue: 'А',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyG: {
+      value: 'п',
+      altValue: 'П',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyH: {
+      value: 'р',
+      altValue: 'Р',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyJ: {
+      value: 'о',
+      altValue: 'О',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyK: {
+      value: 'л',
+      altValue: 'Л',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyL: {
+      value: 'д',
+      altValue: 'Д',
+      isCapitals: false,
+    },
+  },
+  {
+    Semicolon: {
+      value: 'ж',
+      altValue: 'Ж',
+      isCapitals: false,
+    },
+  },
+  {
+    Quote: {
+      value: 'э',
+      altValue: 'Э',
+      isCapitals: false,
+    },
+  },
+  {
+    Enter: {
+      value: 'Enter',
+      altValue: 'Enter',
+      isCapitals: false,
+    },
+  },
+  {
+    ShiftLeft: {
+      value: 'Shift',
+      altValue: 'Shift',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyZ: {
+      value: 'я',
+      altValue: 'Я',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyX: {
+      value: 'ч',
+      altValue: 'Ч',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyC: {
+      value: 'с',
+      altValue: 'С',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyV: {
+      value: 'м',
+      altValue: 'М',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyB: {
+      value: 'и',
+      altValue: 'И',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyN: {
+      value: 'т',
+      altValue: 'Т',
+      isCapitals: false,
+    },
+  },
+  {
+    KeyM: {
+      value: 'ь',
+      altValue: 'Ь',
+      isCapitals: false,
+    },
+  },
+  {
+    Comma: {
+      value: 'б',
+      altValue: 'Б',
+      isCapitals: false,
+    },
+  },
+  {
+    Period: {
+      value: 'ю',
+      altValue: 'Ю',
+      isCapitals: false,
+    },
+  },
+  {
+    Slash: {
+      value: '.',
+      altValue: ',',
+      isCapitals: false,
+    },
+  },
+  {
+    ArrowUp: {
+      value: '&uarr;',
+      altValue: '&uarr;',
+      isCapitals: false,
+    },
+  },
+  {
+    ShiftRight: {
+      value: 'Shift',
+      altValue: 'Shift',
+      isCapitals: false,
+    },
+  },
+  {
+    ControlLeft: {
+      value: 'Ctrl',
+      altValue: 'Ctrl',
+      isCapitals: false,
+    },
+  },
+  {
+    MetaLeft: {
+      value: 'Win',
+      altValue: 'Win',
+      isCapitals: false,
+    },
+  },
+  {
+    AltLeft: {
+      value: 'Alt',
+      altValue: 'Alt',
+      isCapitals: false,
+    },
+  },
+  {
+    Space: {
+      value: 'Space',
+      altValue: 'Space',
+      isCapitals: false,
+    },
+  },
+  {
+    AltRight: {
+      value: 'Alt',
+      altValue: 'Alt',
+      isCapitals: false,
+    },
+  },
+  {
+    ArrowLeft: {
+      value: '&larr;',
+      altValue: '&larr;',
+      isCapitals: false,
+    },
+  },
+  {
+    ArrowDown: {
+      value: '&darr;',
+      altValue: '&darr;',
+      isCapitals: false,
+    },
+  },
+  {
+    ArrowRight: {
+      value: '&rarr;',
+      altValue: '&rarr;',
+      isCapitals: false,
+    },
+  },
+  {
+    ControlRight: {
+      value: 'Ctrl',
+      altValue: 'Ctrl',
+      isCapitals: false,
+    },
+  },
+];
+
+let keyboardLayout = false;
+let initialLanguage = keysEng;
+let capslock = false;
+
+const root = document.createElement('div');
+root.classList.add('root');
+
+const capsLockIndicator = document.createElement('div');
+capsLockIndicator.classList.add('capslock-indicator');
+
+const textArea = document.createElement('textarea');
+textArea.classList.add('text-area');
+textArea.maxLength = 100;
+textArea.rows = 10;
+
+const keyBox = document.createElement('div');
+keyBox.classList.add('key-box');
+
+root.append(capsLockIndicator, textArea, keyBox);
+document.body.append(root);
+
+function render() {
+  initialLanguage.forEach((keys) => {
+    for (const [button, buttonValue] of Object.entries(keys)) {
+      const divKey = document.createElement('div');
+      divKey.classList.add(button.toLowerCase(), 'key');
+      divKey.innerHTML = `${
+        capslock ? buttonValue.altValue : buttonValue.value
+      }`;
+      keyBox.append(divKey);
+    }
+  });
+}
+render(keyboardLayout);
